4c1fb3b84e3403b508083d581d1199a7a0bbf915
achmad_khusnul_yakin <digivoraofficial81@gmail.com>
2026-03-11 10:07:57 +0700

    first commit

---
+++ src/perkenalan_singkat/Perkenalan_Singkat.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package perkenalan_singkat;
+
+/**
+ *
+ * @author achmad_khusnul_yakin
+ */
+public class Perkenalan_Singkat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Assalamualaikum Wr.Wb");
+        System.out.println("Selamat Pagi, Perkenalkan Saya!");
+        System.out.println("Nama        \t\t: Achmad Khusnul Yakin");
+        System.out.println("Jenis Kelamin   \t: Laki-laki");
+        System.out.println("Tempat,Tanggal Lahir    : Jombang, 30 September 2006");
+        System.out.println("Umur    \t\t: 19 Tahun");
+        System.out.println("Profesi \t\t: Mahasiswa");
+        System.out.println("Fakultas    \t\t: Teknologi");
+        System.out.println("Prodi   \t\t: Sistem Informasi");
+        System.out.println("Alamat  \t\t: Nganjuk,Jawa Timur");
+        System.out.println("No. WhatshApp \t\t: 0831-1615-2891");
+    }
+    
+}
